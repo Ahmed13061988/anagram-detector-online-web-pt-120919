b9842d960c418d6535eb64b198b2e3f133b18d2d
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-15 19:44:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,15 @@
 # Your code goes here!
-class Anagram 
+class Anagram
+  
 attr_accessor :word 
  
  def initialize(word)
    @word = word 
  end
-
-
-
-
-
-
+ def match 
+  match.each do |a|
+    a.split (" ")
+end 
+end 
 
 end 
